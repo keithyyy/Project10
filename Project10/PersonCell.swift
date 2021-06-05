d1c348b4cbfd73fabde5cdab55409bbf7aadbd71
Keith Crooc <keith.crooc@gmail.com>
2021-06-05 17:04:59 -0400

    day42 - setting up collectionViewController and PersonCell. A step forward passed tableview, replacing rows with index/cells.

---
+++ Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Keith Crooc on 2021-06-05.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
--- Project10/ViewController.swift
+++ Project10/ViewController.swift
@@ -7,11 +7,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        notice that without the guard, our cell shows up as a UICollectionView cell - and not our PersonCell. Meaning we can't access it's properties.
+//        To workaround that, we need to typecast it using GUARD.
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("unable to deQueue PersonCell")
+        }
+        return cell
     }
 
 
